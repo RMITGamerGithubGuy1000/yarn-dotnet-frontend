1498f14ef0217338c13d9d14664f960e58330e31
RMITGamerGithubGuy1000 <marcussamuelbutera@hotmail.com>
2025-09-21 13:55:09 +1000

    Added SSR support

--- packages/site/src/index.tsx
+++ packages/site/src/index.tsx
@@ -1,12 +1,23 @@
-import App from "./App";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-const root = createRoot(document.getElementById("root")!);
+import App from "./App";
+import { createStore } from "@frontend/state";
+
+const preloadedState =
+  typeof window !== "undefined" ? (window as any).__PRELOADED_STATE__ : undefined;
+
+const store = createStore(preloadedState);
+
+const container = document.getElementById("root")!;
+const root = createRoot(container);
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
--- packages/state/src/store.ts
+++ packages/state/src/store.ts
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { rootReducer } from "./rootReducer";
+import { rootReducer, RootState } from "./rootReducer";
 
-export const createStore = (preloadedState?: Partial<{ cookies: { count: number } }>) =>
+export const createStore = (preloadedState?: Partial<RootState>) =>
   configureStore({
     reducer: rootReducer,
     preloadedState,
   });
 
-export type AppDispatch = ReturnType<typeof createStore>["dispatch"];
+export type AppStore = ReturnType<typeof createStore>;
+export type AppDispatch = AppStore["dispatch"];
